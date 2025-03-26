ac0bcf2d17db4d1d9d0f973c5c1a9b13986b7a24
samirshivji <39103564+samirshivji@users.noreply.github.com>
2025-03-25 19:00:43 -0700

    Update tictactoe.py: implemented player, actions, result, winner, terminal, utility, and minimax functions

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,46 +23,150 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    countX = 0
+    countO = 0
+
+    for row in range(len(board)):
+        for col in range (len(board[row])):
+            if board[row][col] == X:
+                countX += 1
+            if board[row][col] == O:
+                countO += 1
+
+    if countX > countO:
+        return O
+    else:
+        return X
+    
+
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    allPossibleActoins = set()
+
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == EMPTY:
+                allPossibleActoins.add((row, col))
+
+    return allPossibleActoins
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
 
+    if action not in actions(board):
+        raise Exception("Not Valid Action")
+    
+    boardCopy = copy.deepcopy(board)
+    row, col = action 
+
+    boardCopy[row][col] = player(board)
+
+    return boardCopy 
+
+
+def checkRow(board, player):
+    for row in range(len(board)):
+        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            return True
+    return False
+
+def checkCol(board, player):
+    for col in range(len(board)):
+        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            return True
+    return False
+
+def checkDiag(board, player):
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    return False
+                     
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    if checkRow(board, X) or checkCol(board, X) or checkDiag(board, X):
+        return X
+    elif checkRow(board, O) or checkCol(board, O) or checkDiag(board, O):
+        return O
+    else:
+        return None
+
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X or winner(board) == O:
+        return True
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == EMPTY:
+                return False
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else: 
+        return 0 
+
+
+def maxValue(board):
+    v = -math.inf
+
+    if terminal(board):
+        return utility(board)
+    for action in actions(board):
+        v = max(v, minValue(result(board, action)))
+    return v
+
+def minValue(board):
+    v = math.inf
+
+    if terminal(board):
+        return utility(board)
+    for action in actions(board):
+        v = max(v, minValue(result(board, action)))
+    return v
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    #x --> max player
+    #O --> min player 
+
+    if terminal(board):
+        return None
+    
+    elif player(board) == X:
+        plays = []
+        for action in actions(board):
+            plays.append([minValue(result(board, action)), action])
+        return sorted(plays, key=lambda x: x[0], reverse = True)[0][1]
+    
+    elif player(board) == O:
+        plays = []
+        for action in actions(board):
+            plays.append([maxValue(result(board, action)), action])
+        return sorted(plays, key=lambda x: x[0])[0][1]
+
